babbed08295061eca6061e8bc6a6a343bb1a6390
zzgyy123 <zzgyy123@163.com>
2017-10-18 18:17:27 +0800

    Chaptor10

---
+++ Assets/Editor/MaterialInstancer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MaterialInstancer : Editor
+{
+
+    [MenuItem("GameObject/Instance Material")]
+    public static void Instance()
+    {
+        if (Selection.activeGameObject == null || Selection.activeGameObject.GetComponent<MeshRenderer>() == null)
+        {
+            Debug.LogError("No Valid Object Selected");
+            return;
+        }
+
+        Material mat = Selection.activeGameObject.GetComponent<MeshRenderer>().sharedMaterial;
+        Selection.activeGameObject.GetComponent<MeshRenderer>().sharedMaterial = new Material(mat);
+        Selection.activeGameObject.GetComponent<MeshRenderer>().sharedMaterial.name = Selection.activeGameObject.GetComponent<MeshRenderer>().sharedMaterial.name + "(InstanceByCode)";
+    }
+
+}
---
+++ Assets/Scripts/SelfTest/TestSharedMaterial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSharedMaterial : MonoBehaviour
+{
+
+    Renderer myRenderer;
+
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        myRenderer.material.color = Color.red;
+        //myRenderer.sharedMaterial.color = Color.blue;
+    }
+}
